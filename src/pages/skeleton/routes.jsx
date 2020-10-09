a0af61c9989944d05bd470835494f134fe598c3e
Дровянникова Ирина Сергеевна <Drovyannikova.I.Se@sberbank.ru>
2020-10-09 11:30:00 +0300

    cards

--- src/pages/skeleton/routes.jsx
+++ src/pages/skeleton/routes.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import paths from './paths';
 import SettingsPage from '../protected/client/settings/settings';
+import Home from '../home';
 
 export default {
   index: {
     path: paths.index,
-    component: () => (<h1>Home</h1>),
+    component: () => <Redirect to={paths.products} />,
+  },
+  home: {
+    path: paths.products,
+    component: Home,
   },
   settings: {
     path: paths.settings,
--- src/pages/skeleton/skeleton.jsx
+++ src/pages/skeleton/skeleton.jsx
@@ -4,53 +4,42 @@ import { SideMenu } from '../../components/sideMenu';
 import routes from './routes';
 import history from '../../history';
 import styled from 'styled-components';
-import {HomeOutline, ListOutline, SettingsOutline} from '../../components/icon';
 import colors from '../../colors';
 
 const StyledMainContainer = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
   height: 100%;
+  flex-direction: column;
 `;
 const StyledContentContainer = styled.div`
 display: flex;
 flex: 1;
 flex-basis: 80%;
 height: 100%;
-padding-top: 1vw;
-padding-right: 1vw;
-padding-bottom: 1vw;
+padding: 1vw;
 border-radius: 1rem;
 background-color: ${colors.BACKGROUND};
 `;
 
 const links = () => {
-  const role = localStorage.getItem('role');
   return [
     {
-      label: role === 'client' ? 'Мои заявки' : 'Мои задачи',
-      path: '/statuses',
-      key: '/statuses',
-      icon: <HomeOutline/>,
-      iconActive: <HomeOutline color={colors.INFO}/>
+      label: 'Мои продукты',
+      path: '/products',
+      key: '/products',
     },
     {
-      label: 'Каталог услуг',
-      path: '/services',
-      key: '/services',
-      icon: <ListOutline/>,
-      iconActive: <ListOutline color={colors.INFO}/>
+      label: 'Переводы',
+      path: '/pays',
+      key: '/pays',
     },
     {
       label: 'Настройки',
       path: '/settings',
       key: '/settings',
-      icon: <SettingsOutline/>,
-      iconActive: <SettingsOutline color={colors.INFO}/>
     },
   ];
-}
+};
 
 const Skeleton = () => (
   <BrowserRouter history={history}>
