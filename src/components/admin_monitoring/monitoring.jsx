a20eff5465b05b4485d7334e9f59603071f8a9a7
Rashid Alkhoev <mr.alhoev@mail.ru>
2020-10-11 14:03:44 +0300

    fix table

--- src/components/admin_monitoring/monitoring.jsx
+++ src/components/admin_monitoring/monitoring.jsx
@@ -1,155 +1,155 @@
-import React from "react";
-import { Table, Tag, Space } from "antd";
-import styles from "./monitoring.module.scss";
+import React from 'react';
+import { Table, Tag, Space } from 'antd';
+import styles from './monitoring.module.scss';
 
 const Monitoring = () => {
   const fakeLogs = [
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:53:10",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:53:10',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:58",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:58',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:32",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:32',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:27",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:27',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:05",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:05',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:05",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:05',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:05",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:05',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 11:50:05",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 11:50:05',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 09:50:05",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 09:50:05',
     },
     {
-      usedId: "1",
-      ip: "46.160.253.84",
+      usedId: '1',
+      ip: '46.160.253.84',
       request_log:
         '{"status":200,"data":{"id":1,"first_name:Ira,last_name:Drov,email":"test@test.com","reset_password_token":null,"reset_password_created_at":null,"stripe_id":"cus_HEWpZuAKEvZJQi","card_brand":null,"card_last_four":null,"trial_ends_at":null,"timezone:Europe\\/Moscow","roles":[],"account":{"id":1,"name":"Test","stores_count":2}}}',
       user_agent:
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36",
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
       fingerprint:
-        "96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8",
-      created_at: "2020-10-11 09:50:05",
+        '96ac79fee9c502d6ad996d36eb86f483ec12f0a30b99098aac42cbded89a25a8',
+      created_at: '2020-10-11 09:50:05',
     },
   ];
   const columns = [
     {
-      title: "UserID",
-      dataIndex: "usedId",
-      key: "usedId",
+      title: 'UserID',
+      dataIndex: 'usedId',
+      key: 'usedId',
       width: 5,
     },
     {
-      title: "IP",
-      dataIndex: "ip",
-      key: "ip",
+      title: 'IP',
+      dataIndex: 'ip',
+      key: 'ip',
       width: 10,
     },
     {
-      title: "Лог запросов",
-      dataIndex: "request_log",
-      key: "request_log",
+      title: 'Лог запросов',
+      dataIndex: 'request_log',
+      key: 'request_log',
       width: 25,
     },
     {
-      title: "User agent",
-      dataIndex: "user_agent",
-      key: "user_agent",
+      title: 'User agent',
+      dataIndex: 'user_agent',
+      key: 'user_agent',
       width: 20,
     },
     {
-      title: "Слепок пользователя",
-      dataIndex: "fingerprint",
-      key: "fingerprint",
+      title: 'Слепок пользователя',
+      dataIndex: 'fingerprint',
+      key: 'fingerprint',
       width: 10,
     },
     {
-      title: "Время создания",
-      dataIndex: "created_at",
-      key: "created_at",
+      title: 'Время создания',
+      dataIndex: 'created_at',
+      key: 'created_at',
       width: 10,
     },
   ];
@@ -157,9 +157,10 @@ const Monitoring = () => {
     <div className={styles.tab}>
       <Table
         className={styles.scroll}
-        scroll={{ y: 500 }}
+        scroll={{ y: 800 }}
         columns={columns}
         dataSource={fakeLogs}
+        pagination={false}
       />
     </div>
   );
